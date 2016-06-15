7d362c4f6fd26ed3710ec450a006efb33dc8fe1e
Erick Chacon Montalvan <eral.th07@gmail.com>
2016-06-15 23:42:44 +0100

    db-summary and get_path functions

---
+++ R/db-summary.R
@@ -0,0 +1,33 @@
+#' @title Summarize the databases inside a folder.
+#' 
+#' @description
+#' \code{db_summary} write the summary of databases, founded inside a provided
+#' folder, into a file.
+#' 
+#' @details
+#' When the filename is not specified, \code{db_summary} will create the file
+#' "summary-databases.txt".
+#' 
+#' @param file file name to look for.
+#' 
+#' @return A character indicating the folder path.
+#' 
+#' @author Erick Albacharro Chacon-Montalvan
+db_summarize <- function(path_data, filename = "summary-databases.txt"){
+  databases <- list.files(path_data,".RData$")
+  write("------------------------------------------------", filename)
+  write("Summary of databases inside the provided folder.", filename, app = T)
+  write("------------------------------------------------", filename, app = T)
+  for(i in 1:length(databases)){
+    write(paste0("\n", i, ") RData file: ", databases[i]), filename, app = T)
+    obj_names <- load(paste0(path_data, databases[i]))
+    for(j in 1:length(obj_names)){
+      var_names <- eval(parse(text = paste("names(", obj_names[j],")")))
+      eval(parse(text = paste0("rm(list = '", obj_names[j], "')")))
+      if(!is.null(var_names)){
+      write(paste0(i, ".", j, ") Object: ", obj_names[j]), filename, app = T)
+      write(paste(var_names, collapse = " "), filename, app = T)
+      }
+    }
+  }
+}
---
+++ R/get-path.R
@@ -0,0 +1,20 @@
+#' @title Get the folder path containing file.
+#' 
+#' @description
+#' \code{get_path} returns the folder path that contains a specified file.
+#' 
+#' @details
+#' When the file name is not specified, \code{get_path} will look for the
+#' file "doctoral-research.ecm".
+#' 
+#' @param file file name to look for.
+#' 
+#' @return A character indicating the folder path.
+#' 
+#' @author Erick Albacharro Chacon-Montalvan
+get_path <- function(file = "doctoral-research.ecm") {
+  full_path <- system(paste('find ~/ -name', file, '2>/dev/null'), intern = TRUE)
+  path <- gsub("doctoral-research.ecm$", "", full_path)
+  return(path)
+}
+
