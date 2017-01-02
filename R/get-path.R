5a30dccadca7db6d979bce69b4514f12a232bbec
Erick Chacon <eral.th07@gmail.com>
2017-01-02 23:55:41 +0000

    remove whitespace

--- R/get-path.R
+++ R/get-path.R
@@ -1,16 +1,16 @@
 #' @title Get the folder path containing file.
-#' 
+#'
 #' @description
 #' \code{get_path} returns the folder path that contains a specified file.
-#' 
+#'
 #' @details
 #' When the file name is not specified, \code{get_path} will look for the
 #' file "doctoral-research.ecm".
-#' 
+#'
 #' @param file file name to look for.
-#' 
+#'
 #' @return A character indicating the folder path.
-#' 
+#'
 #' @author Erick Albacharro Chacon-Montalvan
 #'
 #' @export
