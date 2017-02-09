b4bcd6090ea2b37ed849e276fa7993852a97a934
Erick Chacon <eral.th07@gmail.com>
2017-02-09 13:20:00 +0000

    ggplot functions for wrapping text and scale sequence

--- R/gg-adds.R
+++ R/gg-adds.R
@@ -1,4 +1,16 @@
-
+#' @title Wrap text for ggplots.
+#'
+#' @description
+#' \code{wrapit} wrap long texts to be used with ggplot package.
+#'
+#' @param text long text to be wrapped
+#' @param width width of the wrap
+#'
+#' @return wrapped text
+#'
+#' @author Erick A. Chacon-Montalvan
+#'
+#' @export
 wrapit <- function(text, width = 20) {
   wtext <- paste(strwrap(text, width = width), collapse=" \n ")
   return(wtext)
@@ -41,4 +53,26 @@ condense2 <- function (x, y, var = NULL, data, width, origin, ...) {
     zbin <- substitute(condense(xbin, ybin, ...))
   }
   return(eval(zbin, data))
+}
+
+#' @title Creates sequence for continuous scale .
+#'
+#' @description
+#' \code{ggscale_seq} Creates a function that can be provide to compute
+#' break for a continuous scale axis.
+#'
+#' @details
+#' details.
+#'
+#' @param by desired step of the sequence as in \code{\link{seq}}.
+#'
+#' @return a function that can be provided to \code{\link{scale_x_continuos}}
+#' or similar ones.
+#'
+#' @author Erick A. Chacon-Montalvan
+#'
+#' @export
+ggscale_seq <- function (by = 1) {
+  fun <- function (x) seq(ceiling(min(x)), floor(max(x)), by = by)
+  return(fun)
 }
