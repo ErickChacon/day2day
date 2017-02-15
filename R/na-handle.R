26c83539630da06ce87439282210971351b7b241
Erick Chacon <eral.th07@gmail.com>
2017-02-15 23:49:50 +0000

    mean_na fun

---
+++ R/na-handle.R
@@ -0,0 +1,36 @@
+#' @title Sum vector with NA values.
+#'
+#' @description
+#' \code{sum_na} Sum vector with NA values.
+#'
+#' @details
+#' details.
+#'
+#' @param x
+#' @param ...
+#'
+#' @author Erick A. Chacon-Montalvan
+#'
+#' @export
+sum_na <- function (x, ...) {
+  sum(x, na.rm = TRUE, ...)
+}
+
+
+#' @title Sum vector with NA values.
+#'
+#' @description
+#' \code{mean_na} Mean vector with NA values.
+#'
+#' @details
+#' details.
+#'
+#' @param x
+#' @param ...
+#'
+#' @author Erick A. Chacon-Montalvan
+#'
+#' @export
+mean_na <- function (x, ...) {
+  mean(x, na.rm = TRUE, ...)
+}
