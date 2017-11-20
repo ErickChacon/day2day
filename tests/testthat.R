214f7a1c6f991986af5134c5ace5f2a540f28bf5
Erick Chacon Montalvan <eral.th07@gmail.com>
2017-11-20 14:19:21 +0000

    first testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,3 @@ library(day2day)
 
 test_check("day2day")
 
-# test_that("check", {
-#   expect_equal(sum(1:10), 55)
-# })
---
+++ tests/testthat/test-test.R
@@ -0,0 +1,6 @@
+context("test")
+
+test_that("First test", {
+  expect_equal(sum(1:10), 55)
+})
+
