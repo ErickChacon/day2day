01b272399657f63c2e18c51c7e57454cf4eb4644
Erick Chacon Montalvan <eral.th07@gmail.com>
2017-11-20 13:28:45 +0000

    check travis

--- R/day2day.R
+++ R/day2day.R
@@ -9,6 +9,7 @@
 #' @name day2day
 NULL
 
+# added travis
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 # if(getRversion() >= "2.15.1") utils::globalVariables(c("."))
 if(getRversion() >= "2.15.1") utils::globalVariables(
